ed8ac8a38fb18b1289442d574d1ebbdf28107dcc
liridon.rama <liridon.rama@me.com>
2020-10-24 09:55:01 +0200

    a second example of interfaces

--- main.go
+++ main.go
@@ -2,6 +2,36 @@ package main
 
 import "fmt"
 
+type person struct {
+	first string
+}
+
+type secretAgent struct {
+	person
+	ltk bool
+}
+
+func (p person) speak() {
+	fmt.Println("I'm a person. this is my name", p.first)
+}
+
+func (s secretAgent) speak() {
+	fmt.Println("I'm a secret agent — this is my name: ", s.first)
+}
+
+type human interface {
+	speak()
+}
+
 func main() {
-	fmt.Println("Hello World")
+	p1 := person{
+		first: "Miss Moneypenny",
+	}
+	s1 := secretAgent{person: p1, ltk: true}
+
+	var x, y human
+	x, y = p1, s1
+
+	x.speak()
+	y.speak()
 }
