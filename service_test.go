729ab1b6ce3df3cd95097d962d755275ab4ecbd0
liridon.rama <liridon.rama@me.com>
2020-11-06 16:13:11 +0100

    hi

--- service_test.go
+++ service_test.go
@@ -3,6 +3,8 @@ package architecture
 import (
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 type Db map[int]Person
@@ -16,16 +18,20 @@ func (d Db) Retrieve(n int) Person {
 }
 
 func TestPut(t *testing.T) {
+	assert := assert.New(t)
 	db := Db{}
 	p := Person{
 		First: "James",
 	}
 
+	p2 := Person{
+		First: "hans",
+	}
+
 	Put(db, 1, p)
 
-	if db.Retrieve(1) != p {
-		t.Fatalf("Want %v, got %v", p, p)
-	}
+	assert.Equal(db.Retrieve(1), p, "should be the same person")
+	assert.NotEqual(db.Retrieve(1), p2, "should not be equal")
 }
 
 func ExamplePut() {
