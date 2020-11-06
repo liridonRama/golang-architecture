0b6adbfac004759194519d8239110b2262e89d6f
liridon.rama <liridon.rama@me.com>
2020-11-06 15:51:44 +0100

    added tests and example

--- service.go
+++ service.go
@@ -4,19 +4,23 @@ import (
 	"fmt"
 )
 
+// Person does stuff
 type Person struct {
 	First string
 }
 
+// Accessor is an interesting interface
 type Accessor interface {
 	Save(n int, p Person)
 	Retrieve(n int) Person
 }
 
+// PersonService is nice
 type PersonService struct {
 	A Accessor
 }
 
+// Get gets stuff from db
 func (ps PersonService) Get(n int) (Person, error) {
 	p := ps.A.Retrieve(n)
 	if p.First == "" {
@@ -26,10 +30,12 @@ func (ps PersonService) Get(n int) (Person, error) {
 	return p, nil
 }
 
+// Put adds stuff to the db
 func Put(a Accessor, n int, p Person) {
 	a.Save(n, p)
 }
 
+// Get gets it
 func Get(a Accessor, n int) Person {
 	return a.Retrieve(n)
 }
